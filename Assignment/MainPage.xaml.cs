1a79067351584a31cebedc72252cfd59e360a30a
1821913ucol <54563901+1821913ucol@users.noreply.github.com>
2019-10-15 14:15:26 +1300

    Update login page and navigation

--- Assignment/MainPage.xaml.cs
+++ Assignment/MainPage.xaml.cs
@@ -16,14 +16,13 @@ using System.Windows.Shapes;
 namespace Assignment
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainPage : Page
     {
-        public MainWindow()
+        public MainPage()
         {
             InitializeComponent();
-            MainFrame.NavigationService.Navigate(new LoginPage());
         }
     }
 }
