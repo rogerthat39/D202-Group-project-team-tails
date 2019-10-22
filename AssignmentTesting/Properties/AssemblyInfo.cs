4d57eedd2e5507ebffde6759fbc362801e9528ed
1821913ucol <54563901+1821913ucol@users.noreply.github.com>
2019-10-22 15:55:59 +1300

    add unit test files sprint 3 week 2

---
+++ AssignmentTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AssignmentTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AssignmentTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7750632-c34c-495b-aef1-61556ae9397f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AssignmentTesting/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment;
+
+namespace AssignmentTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestdoesMajorExist()
+        {
+            MainPage obj = new MainPage();
+
+            obj.getMajors();
+        }
+    }
+}
