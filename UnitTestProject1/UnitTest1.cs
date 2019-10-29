af214d6d86f67d165e847f614e1f66e891c068c6
Amy Rogers <47173916+rogerthat39@users.noreply.github.com>
2019-10-29 13:44:26 +1300

    Unit test files

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            /*List<Paper> academicHistory = new List<Paper>
+            {
+                new Paper("D111", "Database Fundamentals", null, 1, new List<int> {1}, null, true, 15),
+                new Paper("T111", "Computer Hardware Fundamentals", null, 1, new List<int> {1}, null, true, 15),
+                new Paper("I101", "Information System Fundamentals", null, 1, new List<int> {1}, null, true, 15)
+            };*/
+
+            /*List<Paper> paperList = new List<Paper>
+                {
+                    new Paper("D211", "Database Development", null, 2, new List<int> {1}, new List<String> {"D111"}, true, 15),
+                    new Paper("T211", "Security", null, 2, new List<int> {1}, new List<String> {"T111"}, true, 15),
+                    new Paper("D111", "Database", null, 1, new List<int> {1}, null, true, 15),
+                    new Paper("I203", "Information System Fundamentals", null, 2, new List<int> {1}, new List<String> {"I101"}, false, 15),
+                    new Paper("I209", "Industry Placement", null, 2, new List<int> {2}, new List<String> {"I567"}, false, 15)
+                };*/
+
+            //for testing recommend papers:
+            //"D211, T211" should be added
+            //d111 is a first year paper
+        }
+    }
+}
