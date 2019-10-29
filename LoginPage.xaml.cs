436215c1dfb989f37034303cbe27bc95e767c6ec
Amy Rogers <47173916+rogerthat39@users.noreply.github.com>
2019-10-29 16:05:11 +1300

    Admin Page and QOL updates
    - connection manager
    - add feedback when adding paper to academic history
    - admin login goes to admin page
    - show papers on admin page
    - (unfinished) data changed in datagrid is updated in database

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        ConnectionManager cm = new ConnectionManager();
+
+        private void clkLogin(object sender, RoutedEventArgs e)
+        {
+            //open connection to database
+            SqlCommand cnd = cm.Open("select * from users where username = @u and password = @p");
+
+            //add parameters
+            SqlParameter username = cnd.Parameters.Add("@u", SqlDbType.VarChar);
+            SqlParameter password = cnd.Parameters.Add("@p", SqlDbType.VarChar);
+            username.Value = txtBoxUserName.Text;
+            password.Value = txtBoxPassword.Text;
+
+            //execute query
+            SqlDataReader reader = cnd.ExecuteReader();
+
+            //check if there was a matching record in DB
+            if (reader.Read())
+            {
+                //checking case of username and password
+                if (txtBoxUserName.Text.Equals(reader[0].ToString().TrimEnd()) && txtBoxPassword.Text.Equals(reader[1].ToString().TrimEnd()))
+                {
+                    //if role = admin, navigate to admin page
+                    if (reader[2].ToString() == "admin")
+                    {
+                        this.NavigationService.Navigate(new AdminPage());
+                    }
+                    else //if role = student, navigate to the main page
+                    { 
+                        this.NavigationService.Navigate(new MainPage());
+                    }
+                }
+                else
+                {
+                    //displays if the username or password had incorrect case
+                    MessageBox.Show("Incorrect username or password");
+                }
+            }
+            else
+            {
+                //displays if there was no matching record in DB
+                MessageBox.Show("Incorrect username or password");
+            }
+
+            cm.Close();
+        }
+    }
+}
---
+++ Paper.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class Paper: IComparable<Paper>
+    {
+        private string code;
+        private string name;
+        private int year;
+        private List<int> semesters;
+        private List<String> prerequisites;
+        private bool compulsory;
+        private int credits;
+        private string description;
+
+        public string Code { get => code; set => code = value; }
+        public string Name { get => name; set => name = value; }
+        public int Year { get => year; set => year = value; }
+        public List<int> Semesters { get => semesters; set => semesters = value; }
+        public List<String> Prerequisites { get => prerequisites; set => prerequisites = value; }
+        public bool Compulsory { get => compulsory; set => compulsory = value; }
+        public int Credits { get => credits; set => credits = value; }
+        public string Description { get => description; set => description = value; }
+
+        public Paper(string code, string n, string d, int y, List<int> s, List<String> p, bool c, int i)
+        {
+            Code = code;
+            Name = n;
+            Year = y;
+            Semesters = s;
+            Prerequisites = p;
+            Compulsory = c;
+            Credits = i;
+            Description = d;
+        }
+
+        public override string ToString()
+        {
+            if (this.Semesters.Count == 2)
+            {
+                return code + " " + name + " -- Semesters: " + Semesters[0] + " and " + Semesters[1] + " --Year: " + Year;
+            }
+            else
+            {
+                return code + " " + name + " -- Semester: " + Semesters[0] + " --Year: " + Year;
+            }
+        }
+
+        public int CompareTo(Paper otherPaper)
+        {
+            //check the year and semester are not the same
+            if(this.Year == otherPaper.Year && this.Semesters == otherPaper.Semesters)
+            {
+                return 0;
+            }
+            //display papers with lowest year first
+            else if(this.Year > otherPaper.Year)
+            {
+                return 1;
+            }
+            else if (this.Year < otherPaper.Year)
+            {
+                return -1;
+            }
+            //if one paper has two semesters, it should be sorted below a paper with one semester
+            else if(this.Semesters.Count == 2 && otherPaper.Semesters.Count == 1)
+            {
+                return 1;
+            }
+            else if (this.Semesters.Count == 1 && otherPaper.Semesters.Count == 2)
+            {
+                return -1;
+            }
+            //display semesters with lowest number first
+            else if (this.Semesters[0] > otherPaper.Semesters[0])
+            {
+                return 1;
+            }
+            else if (this.Semesters[0] < otherPaper.Semesters[0])
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
